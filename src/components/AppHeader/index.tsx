284059268f530e9269c47a82bd7872228ed97ecc
namxg6626 <namxg6626@gmail.com>
2022-02-22 00:30:24 +0700

    add AppHeader

---
+++ src/components/AppHeader/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {FC} from 'react';
+import {Box, HStack, Text, Pressable} from 'native-base';
+import {Colors} from '@theme/colors';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+export interface IAppHeaderProps {
+  headerContent: React.ReactNode;
+  onBackPress?: () => void;
+}
+
+export const AppHeader: FC<IAppHeaderProps> = ({
+  headerContent,
+  onBackPress,
+}) => {
+  const renderHeaderContent = () => {
+    if (typeof headerContent === 'string') {
+      return (
+        <HStack w={'full'} alignItems={'center'} space={2}>
+          <Pressable
+            style={{
+              transform: [
+                {
+                  scale: 1.25,
+                },
+              ],
+            }}
+            onPress={onBackPress}>
+            <MaterialIcons name="arrow-back-ios" color={'white'} size={24} />
+          </Pressable>
+          <Text fontSize={'4xl'} fontWeight={'bold'}>
+            {headerContent}
+          </Text>
+        </HStack>
+      );
+    }
+
+    return headerContent;
+  };
+
+  return <Box backgroundColor={Colors.gumental}>{renderHeaderContent()}</Box>;
+};
--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -5,16 +5,41 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {Colors} from '@theme/colors';
 import {widthPercentageToDP} from 'react-native-responsive-screen';
 import {Platform, StatusBar} from 'react-native';
+import {AppHeader} from '@components/AppHeader';
+import {useNavigation} from '@react-navigation/native';
+
 export interface IScreenProps {
   disableSafeArea?: boolean;
   enableStatusBar?: boolean;
+  headerContent?: React.ReactNode;
+  onBackPress?: () => void;
 }
 
 export const Screen: FC<IScreenProps> = ({
   children,
   disableSafeArea,
   enableStatusBar = true,
+  headerContent,
+  onBackPress,
 }) => {
+  const navigation = useNavigation();
+
+  const handleBackPress = () => {
+    if (onBackPress) {
+      onBackPress();
+    }
+    navigation.goBack();
+  };
+
+  const renderAppHeader = () => {
+    if (!headerContent) {
+      return null;
+    }
+    return (
+      <AppHeader onBackPress={handleBackPress} headerContent={headerContent} />
+    );
+  };
+
   return (
     <Box
       pt={Platform.OS === 'android' ? 12 : 8}
@@ -22,7 +47,15 @@ export const Screen: FC<IScreenProps> = ({
       backgroundColor={Colors.gumental}
       flex={1}>
       <StatusBar hidden={!enableStatusBar} backgroundColor={Colors.gumental} />
-      {disableSafeArea ? children : <SafeAreaView>{children}</SafeAreaView>}
+      {disableSafeArea && renderAppHeader()}
+      {disableSafeArea ? (
+        children
+      ) : (
+        <SafeAreaView>
+          {renderAppHeader()}
+          {children}
+        </SafeAreaView>
+      )}
     </Box>
   );
 };
