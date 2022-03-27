dbcae9d4bf3edf45b8e1d209cc8375ca8681e7b4
namxg6626 <namxg6626@gmail.com>
2022-03-27 23:51:43 +0700

    dismiss keyboard on press outside

--- src/components/KeyboardAvoidingView/index.tsx
+++ src/components/KeyboardAvoidingView/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import {TouchableWithoutFeedback, Keyboard, StyleSheet} from 'react-native';
 import {Box, KeyboardAvoidingView as NBKeyboardAvoidingView} from 'native-base';
 import {Platform} from 'react-native';
 
-export const KeyboardAvoidingView: React.FC<any> = ({children}) => {
+export const KeyboardAvoidingView: React.FC = ({children}) => {
   return (
     <NBKeyboardAvoidingView
       behavior={Platform.OS === 'android' ? 'height' : 'padding'}
       flex={1}>
-      <Box flex={1} justifyContent={'flex-end'}>
-        {children}
-      </Box>
+      <TouchableWithoutFeedback
+        style={styles.touchableWithoutFeedback}
+        onPress={Keyboard.dismiss}>
+        <Box flex={1} justifyContent={'flex-end'}>
+          {children}
+        </Box>
+      </TouchableWithoutFeedback>
     </NBKeyboardAvoidingView>
   );
 };
+
+const styles = StyleSheet.create({
+  touchableWithoutFeedback: {
+    flex: 1,
+  },
+});
